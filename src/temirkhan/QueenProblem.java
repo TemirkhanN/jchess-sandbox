af514324ce72a1c33732dffb5b8fd9ca7a22dc1a
Temirkhan <diezza.com@gmail.com>
2019-01-27 21:29:27 +0300

    Размечена задача проблемы N ферзей

--- src/temirkhan/QueenProblem.java
+++ src/temirkhan/QueenProblem.java
@@ -2,7 +2,7 @@ package temirkhan;
 
 import java.util.Map;
 
-public class Main {
+public class QueenProblem {
     public static void main(String[] args) {
         Chessboard chessboard = new Chessboard(Map.of());
         chessboard.render();
